ea0c00dbb5ad4b95fa5f60d853ebbd20297b67ec
seirdotexe <80536518+seirdotexe@users.noreply.github.com>
2022-05-07 19:11:08 +0200

    Prepare test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,19 @@
 'use strict';
 
-import test from 'node:test';
-import assert from 'assert';
+import { readdir } from 'fs/promises';
 
-import FormsAuthentication from '../lib/index.js';
+(async () => {
+  const units = await (await readdir('./test/units')).reduce(async (arr, unit) => {
+    const res = await import(`./units/${unit}`);
+
+    if (res) {
+      (await arr).push(res.default);
+    }
+
+    return arr;
+  }, []);
+
+  for (let i = 0; i < units.length; i++) {
+    units[i]();
+  }
+})();
