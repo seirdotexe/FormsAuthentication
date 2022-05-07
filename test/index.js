6ecea78671ecab8ade27ecbb533688e628619508
seirdotexe <80536518+seirdotexe@users.noreply.github.com>
2022-05-07 13:11:56 +0200

    Test changes

--- test/index.js
+++ test/index.js
@@ -4,11 +4,3 @@ import test from 'node:test';
 import assert from 'assert';
 
 import FormsAuthentication from '../lib/index.js';
-
-const ticket = FormsAuthentication.createTicket(
-  'MyTicket',
-  new Date(), //2022-04-30T19:50:19.140Z
-  true,
-  'My user data',
-  'My cookie path'
-);
